154934169e5d0c425afa794451eebb708a18300f
Hennzau <dev@enzo-le-van.fr>
2024-09-20 17:16:50 +0200

    script

--- libraries/script/src/lib.rs
+++ libraries/script/src/lib.rs
@@ -1,14 +1,93 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::process::Stdio;
+use tokio::io::AsyncWriteExt;
+
+async fn fetch_script_curl(url: &str) -> eyre::Result<String> {
+    let curl_output = tokio::process::Command::new("curl")
+        .arg("-LsSf")
+        .arg(url)
+        .stdout(Stdio::piped())
+        .spawn()?
+        .wait_with_output()
+        .await?;
+
+    if !curl_output.status.success() {
+        eyre::bail!("Failed to fetch install script");
+    }
+
+    let script_text = String::from_utf8(curl_output.stdout)?;
+
+    Ok(script_text)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+async fn fetch_script_powershell(url: &str) -> eyre::Result<String> {
+    let curl_output = tokio::process::Command::new("powershell")
+        .arg("-c")
+        .arg(format!("irm {}", url))
+        .stdout(Stdio::piped())
+        .spawn()?
+        .wait_with_output()
+        .await?;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    if !curl_output.status.success() {
+        eyre::bail!("Failed to fetch install script");
     }
+
+    let script_text = String::from_utf8(curl_output.stdout)?;
+
+    Ok(script_text)
+}
+
+pub async fn fetch_script(url: &str) -> eyre::Result<String> {
+    #[cfg(target_os = "windows")]
+    let script = fetch_script_powershell(url).await?;
+    #[cfg(not(target_os = "windows"))]
+    let script = fetch_script_curl(url).await?;
+
+    Ok(script)
+}
+
+async fn execute_script_sh(script: String) -> eyre::Result<()> {
+    let mut sh_process = tokio::process::Command::new("sh")
+        .stdin(Stdio::piped())
+        .spawn()?;
+
+    if let Some(mut stdin) = sh_process.stdin.take() {
+        stdin.write_all(script.as_bytes()).await?;
+    }
+
+    let sh_status = sh_process.wait().await?;
+
+    if !sh_status.success() {
+        eyre::bail!("Failed to install uv");
+    }
+
+    Ok(())
+}
+
+async fn execute_script_powershell(script: String) -> eyre::Result<()> {
+    let mut powershell_process = tokio::process::Command::new("powershell")
+        .arg("-c")
+        .stdin(Stdio::piped())
+        .spawn()?;
+
+    if let Some(mut stdin) = powershell_process.stdin.take() {
+        stdin.write_all(script.as_bytes()).await?;
+    }
+
+    let powershell_status = powershell_process.wait().await?;
+
+    if !powershell_status.success() {
+        eyre::bail!("Failed to install uv");
+    }
+
+    Ok(())
+}
+
+pub async fn execute_script(script: String) -> eyre::Result<()> {
+    #[cfg(target_os = "windows")]
+    execute_script_powershell(script).await?;
+    #[cfg(not(target_os = "windows"))]
+    execute_script_sh(script).await?;
+
+    Ok(())
 }
--- libraries/uv/src/lib.rs
+++ libraries/uv/src/lib.rs
@@ -1,14 +1,52 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::path::PathBuf;
+
+use eyre::ContextCompat;
+use simple_home_dir::home_dir;
+
+pub struct UV {
+    pub installed: bool,
+    pub bin: Option<PathBuf>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl UV {
+    pub fn new() -> eyre::Result<Self> {
+        let home = home_dir().wrap_err(eyre::Report::msg("Could not find home directory"))?;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        #[cfg(target_os = "windows")]
+        let bin = home.join(".cargo").join("bin").join("uv.exe");
+        #[cfg(not(target_os = "windows"))]
+        let bin = home.join(".cargo").join("bin").join("uv");
+
+        Ok(Self {
+            installed: bin.exists(),
+            bin: Some(bin),
+        })
+    }
+
+    pub async fn install_bin(&mut self) -> eyre::Result<()> {
+        if !self.installed {
+            #[cfg(target_os = "windows")]
+            let script = script::fetch_script("https://astral.sh/uv/install.ps1").await?;
+            #[cfg(not(target_os = "windows"))]
+            let script = script::fetch_script("https://astral.sh/uv/install.sh").await?;
+
+            script::execute_script(script).await?;
+
+            self.installed = true;
+        }
+
+        Ok(())
+    }
+
+    pub async fn uninstall_bin(&mut self) -> eyre::Result<()> {
+        if self.installed {
+            if let Some(bin) = &self.bin {
+                tokio::fs::remove_file(bin).await?;
+            }
+
+            self.installed = false;
+        }
+
+        Ok(())
     }
 }
