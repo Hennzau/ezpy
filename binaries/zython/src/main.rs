c8c452b41a926cbafaacb26486aec0d1d1126de8
Hennzau <dev@enzo-le-van.fr>
2024-09-22 15:33:01 +0200

    binaries

--- binaries/zython/src/main.rs
+++ binaries/zython/src/main.rs
@@ -1,13 +1,6 @@
-fn main() -> eyre::Result<()> {
-    let rt = tokio::runtime::Runtime::new()?;
-
-    rt.block_on(async {
-        let mut uv = uv::UV::new().unwrap();
-        println!("{:?}", uv.installed);
-        let result = uv.install_bin().await;
-        println!("{:?}", result);
-        println!("{:?}", uv.installed);
-    });
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    println!("Hello, zython!");
 
     Ok(())
 }
--- binaries/zz/src/main.rs
+++ binaries/zz/src/main.rs
@@ -1,15 +1,6 @@
-mod zz;
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    println!("Hello, zz!");
 
-fn main() -> eyre::Result<()> {
-    let terminal = ratatui::init();
-
-    let app_result = zz::App {
-        state: zz::AppState::Running,
-        selected_tab: zz::SelectedTab::new_python()?,
-    }
-    .run(terminal);
-
-    ratatui::restore();
-
-    app_result
+    Ok(())
 }
